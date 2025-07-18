bc9c234e7cfc181f018588247dc58d1807c9fc96
Samarkun <samarkun4@gmail.com>
2025-07-18 18:38:12 +0530

    storage created

--- src/models/server/storage.collection.ts
+++ src/models/server/storage.collection.ts
@@ -2,7 +2,35 @@ import { Permission } from "node-appwrite";
 import { questionAttachmentBucket } from "../name";
 import { storage } from "./config";
 
-
+export default async function getOrCreateStrage() {
+    try {
+        await storage.getBucket(questionAttachmentBucket);
+        console.log("Stroge Connected"); 
+    } catch (error) {
+        try {
+            //try to create the storage
+            await   storage.createBucket(
+                questionAttachmentBucket,
+                questionAttachmentBucket,
+                [
+                    Permission.create("users"),
+                    Permission.read("any"),
+                    Permission.read("users"),
+                    Permission.update("users"),
+                    Permission.delete("users")
+                ],
+                false,
+                undefined,
+                undefined,
+                ["jpg", "png", "gif", "jpeg", "webp", "heic"]
+            );
+            console.log("Storage Created");
+            console.log("Storage Connected")
+        } catch (error) {
+            console.log("Error creating storage:", error) 
+        }
+    } 
+}
 
 
 
