3cf07814fd9ae8aaee045c27edd4669de086bf8e
Samarkun <samarkun4@gmail.com>
2025-07-18 17:18:27 +0530

    set-up frontend and backend

---
+++ src/models/name.ts
@@ -0,0 +1,12 @@
+export const db = 'main-stackden'
+export const questionCollection = 'questions'
+export const answerCollection = 'answers'
+export const commentCollection = 'comments'
+export const voteCollection = 'votes'
+export const questionAttachmentBucket = 'question-attachment'
+
+
+
+
+
+
