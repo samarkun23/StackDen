8409fd3c4685b748fc5c1b1d877865e62952cfec
Samarkun <samarkun4@gmail.com>
2025-07-21 11:38:11 +0530

    decreasing the reputation after del the answer

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -1,12 +1,12 @@
 import { answerCollection, db } from "@/models/name";
-import { databases, users} from "@/models/server/config";
+import { databases, users } from "@/models/server/config";
 import { NextRequest, NextResponse } from "next/server";
 import { ID } from "node-appwrite";
-import {UserPrefs} from '@/store/Auth'
+import { UserPrefs } from '@/store/Auth'
 
-export async function POST(req : NextRequest){
+export async function POST(req: NextRequest) {
     try {
-        const {questionId , answer , autherId} = await req.json()
+        const { questionId, answer, autherId } = await req.json()
 
         const response = await databases.createDocument(db, answerCollection, ID.unique(), {
             content: answer,
@@ -15,7 +15,7 @@ export async function POST(req : NextRequest){
         })
 
         // Increase auther reputation || getPrefs its give you all the prefences that you will work on
-        const perfs = await users.getPrefs<UserPrefs>(autherId) 
+        const perfs = await users.getPrefs<UserPrefs>(autherId)
         await users.updatePrefs(autherId, {
             reputation: Number(perfs.reputation) + 1
         })
@@ -24,39 +24,49 @@ export async function POST(req : NextRequest){
             status: 201
         })
 
-    } catch (error:any) {
+    } catch (error: any) {
         return NextResponse.json(
             {
                 error: error?.message || "Error creating answer"
             },
             {
                 status: error?.status || error?.code || 500
-            } 
-        ) 
+            }
+        )
     }
 }
 
 //all the user may be delete their answer
-export async function DELETE(req:NextRequest) {
+export async function DELETE(req: NextRequest) {
     try {
-        const {answerId} = await req.json()
-        
-        const answer = await databases.getDocument(db, answerCollection , answerId)        
+        const { answerId } = await req.json()
+
+        const answer = await databases.getDocument(db, answerCollection, answerId)
 
         const response = await databases.deleteDocument(db, answerCollection, answerId)
 
-        //decrease the reputation 
-        
+        //decrease the reputation || getPrefs its give you all the prefences that you will work on 
+        const perfs = await users.getPrefs<UserPrefs>(answer.autherId)
+        await users.updatePrefs(answer.autherId, {
+            reputation: Number(perfs.reputation) - 1
+        })
+
+        return NextResponse.json(
+            { data: response},{status:200}
+        )
+
+
+
     } catch (error: any) {
-       return NextResponse.json(
-        {
-            message: error?.message || "Error deleting the answer"
-        },
-        {
-            status: error?.status || error?.code || 500
-        }
-       ) 
-    } 
+        return NextResponse.json(
+            {
+                message: error?.message || "Error deleting the answer"
+            },
+            {
+                status: error?.status || error?.code || 500
+            }
+        )
+    }
 }
 
 
